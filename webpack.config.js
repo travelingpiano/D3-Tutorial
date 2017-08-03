531ac9f021f5100057f600444bece2f3b28a568b
travelingpiano <lihsuanlu@gmail.com>
2017-08-03 09:56:02 -0700

    learning through d3 circle packing

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: "./lib/entry.js",
+  output: {
+  	filename: "./lib/bundle.js"
+  },
+  module: {
+    loaders: [{
+      test: [/\.jsx?$/, /\.js?$/],
+      exclude: /(node_modules)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
+      }
+    }]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
+};
